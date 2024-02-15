f2bf3f874ef70003a2db2370a05b52c18e2a4946
Shane Alcock <shane@alcock.co.nz>
2024-02-15 00:59:44 +0000

    Update copyright statement on all API data fetches

--- src/Response/Envelope.php
+++ src/Response/Envelope.php
@@ -82,7 +82,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 class Envelope
 {
-    const COPYRIGHT = "This data is Copyright (c) 2021 Georgia Tech Research Corporation. All Rights Reserved.";
+    const COPYRIGHT = "This data is Copyright (c) 2021-2024 Georgia Tech Research Corporation. All Rights Reserved.";
 
     /**
      * @Groups({"all", "public"})
